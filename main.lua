06c70d706b6af5e92616629fa9091b893a111c79
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2020-11-17 18:16:32 +0100

    Add player rotation

--- main.lua
+++ main.lua
@@ -1,24 +1,36 @@
+local lg = love.graphics
+
+local player
+local playerX
+local playerY
+local playerRot
+local speed = 150
+
+local windowMode
+
 function love.load()
-  x = 200
-  y = 200
-  speed = 150
-  playerSprite = love.graphics.newImage('Sprites/Player.png')
-  success = love.window.setMode(1280, 720)
+  player = lg.newImage('Sprites/Player.png')
+  playerX = love.graphics.getWidth() / 2
+  playerY = love.graphics.getHeight() / 2
+  playerRot = math.atan2((love.mouse.getY() - playerY), (love.mouse.getX() - playerX))
+
+  windowMode = love.window.setMode(1280, 720)
 end
 
 function love.update(dt)
+  playerRot = math.atan2((love.mouse.getY() - playerY), (love.mouse.getX() - playerX) - 120)
   if love.keyboard.isDown("right", 'd')  then
-    x = x+speed*dt
+    playerX = playerX+speed*dt
   elseif love.keyboard.isDown("left",'a') then
-    x = x - speed * dt
+    playerX = playerX - speed * dt
   end
   if love.keyboard.isDown("down",'s') then
-    y = y+speed*dt
+    playerY = playerY+speed*dt
   elseif love.keyboard.isDown("up",'w') then
-    y = y - speed * dt
+    playerY = playerY - speed * dt
   end
 end
 
 function love.draw()
-  love.graphics.draw(playerSprite, x, y)
+  lg.draw(player, playerX, playerY, playerRot, 1, 1, player:getWidth() / 2, player:getHeight() / 2)
 end
