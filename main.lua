606e256700ae2b90915d8bd59df19e4c672188cf
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2020-11-17 16:54:29 +0100

    Add player movement

--- main.lua
+++ main.lua
@@ -1,3 +1,22 @@
+function love.load()
+  x = 200
+  y = 200
+  speed = 150
+end
+
+function love.update(dt)
+  if love.keyboard.isDown("right", 'd')  then
+    x = x+speed*dt
+  elseif love.keyboard.isDown("left",'a') then
+    x = x - speed * dt
+  end
+  if love.keyboard.isDown("down",'s') then
+    y = y+speed*dt
+  elseif love.keyboard.isDown("up",'w') then
+    y = y - speed * dt
+  end
+end
+
 function love.draw()
-  love.graphics.print("Hello World!")
+  love.graphics.rectangle("fill", x, y, 20, 20)
 end
