62e2cc0dcc500c14191034aa24d84265ecc2473d
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2020-11-17 21:01:22 +0100

    Change some code

--- main.lua
+++ main.lua
@@ -3,22 +3,24 @@ local lg = love.graphics
 local player
 local playerX
 local playerY
-local playerRot
-local speed = 150
+local speed = 300
 
 local windowMode
 
+local screenResW = 1280
+local screenResH = 720
+local playerRot
 function love.load()
   player = lg.newImage('Sprites/Player.png')
   playerX = love.graphics.getWidth() / 2
   playerY = love.graphics.getHeight() / 2
   playerRot = math.atan2((love.mouse.getY() - playerY), (love.mouse.getX() - playerX))
 
-  windowMode = love.window.setMode(1280, 720)
+  windowMode = love.window.setMode(screenResW, screenResH)
 end
 
 function love.update(dt)
-  playerRot = math.atan2((love.mouse.getY() - playerY), (love.mouse.getX() - playerX) - 120)
+  playerRot = math.atan2((love.mouse.getY() - playerY), (love.mouse.getX() - playerX))
   if love.keyboard.isDown("right", 'd')  then
     playerX = playerX+speed*dt
   elseif love.keyboard.isDown("left",'a') then
