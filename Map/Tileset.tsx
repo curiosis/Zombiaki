a5f0572d56e33c722456e713160d8f26c3a1782a
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2021-01-24 02:55:50 +0100

    Add map files

---
+++ Map/Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Tileset" tilewidth="56" tileheight="56" tilecount="340" columns="10">
+ <image source="Tileset.png" width="560" height="1904"/>
+</tileset>
