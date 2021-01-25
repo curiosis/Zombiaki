c36756ec6258d3c795be24fd4331c76f07e85059
Kamil Donda <dondakamil@gmail.com>
2021-01-25 16:09:56 +0100

    Add sound effects

--- Bullet.lua
+++ Bullet.lua
@@ -2,7 +2,8 @@ function Bullet (_sprite)
   local self = {
     sprite = _sprite,
     speed = 500,
-    isVisible = true
+    isVisible = true,
+    shotSound = love.audio.newSource("Audio/Shot.mp3", "static")
   }
 
   -- init position
@@ -45,12 +46,18 @@ function Bullet (_sprite)
     self.sprite.y = self.sprite.y + dy * dirY
   end
 
+  -- sound effects
+  function self.playSoundEffectShot()
+      self.shotSound:setVolume(0.3)
+      self.shotSound:play()
+  end
   return self
 end
 
 function shot(dt)
   local timeOut = (love.timer.getTime() - lastShotTime) * 1000
   if love.mouse.isDown(1) and timeOut >= 500 then
+    Bullet().playSoundEffectShot()
     local bulletSprite = Sprite(bulletImg)
     local bullet = Bullet(bulletSprite)
     bullet.initPosition()
--- Zombie.lua
+++ Zombie.lua
@@ -2,7 +2,9 @@ function Zombie(_sprite)
   local self = {
     sprite = _sprite,
     speed = 100,
-    HP = 100
+    HP = 100,
+    deathSound = love.audio.newSource("Audio/Zombie-death.mp3", "static"),
+    hitSound = love.audio.newSource("Audio/Zombie-hit.mp3", "static")
   }
 
   -- init position
@@ -37,11 +39,25 @@ function Zombie(_sprite)
     return math.atan2((pY - self.sprite.y), (pX - self.sprite.x))
   end
 
+  -- get damage
   function self.getDamage(zombies, i, damage)
     self.HP = self.HP - damage
     if(self.HP <= 0) then
       table.remove(zombies, i)
+      self.playSoundEffectDeath()
+    else
+      self.playSoundEffectHit()
     end
+  end
+
+  -- sound effects
+  function self.playSoundEffectDeath()
+      self.deathSound:setVolume(0.2)
+      self.deathSound:play()
+  end
+  function self.playSoundEffectHit()
+      self.hitSound:setVolume(0.25)
+      self.hitSound:play()
   end
 
   return self
