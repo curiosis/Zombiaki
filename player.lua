8d70b85c7bdc0cdae71311ebdea2820d300ff6e7
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2021-01-26 04:37:29 +0100

    Add camera collider and player collider on map

--- player.lua
+++ player.lua
@@ -13,17 +13,6 @@ player = {
   damage = 100
 }
 
-playerCamera = {
-  m1x = 1040,
-  m1y = 760,
-  m2x = 1040,
-  m2y = 760,
-  m3x = 1320,
-  m3y = 1040,
-  mAx = 1600,
-  mAy = 1880
-}
-
 function player:direction()
   return math.atan2(
   (M.getY() - player.y/(getHeightMap()/camera.resH)),
@@ -51,26 +40,28 @@ end
 
 function player:move(dt)
   local speedDT = self.speed * dt
-  if K.isDown("right", 'd') and self.x < 1626  then self.x = self.x + speedDT
-  elseif K.isDown("left",'a') and self.x > 56 then self.x = self.x - speedDT end
-  if K.isDown("down",'s') and self.y < 839 then self.y = self.y + speedDT
-  elseif K.isDown("up",'w') and self.y > 56 then self.y = self.y - speedDT end
+
+  if K.isDown("right", 'd') then
+    if (m1 and self.x < 1620) or (m2 and self.x < 1620) or (m3 and self.x < 1900) or (mA and self.x < 2180)  then
+      self.x = self.x + speedDT end
+
+  elseif K.isDown("left",'a') and self.x > 60 then
+      self.x = self.x - speedDT end
+
+  if K.isDown("down",'s') then
+    if (m1 and self.y < 840) or (m2 and self.y < 1060) or (m3 and self.y < 1340) or (mA and self.y < 2180) then
+      self.y = self.y + speedDT end
+
+
+  elseif K.isDown("up",'w') then
+    if ((m1 or m2 or mA) and self.y > 60) or (m3 and self.y > 452) then
+      self.y = self.y - speedDT end
+    end
 end
 
 function player:camera()
-  if m1 then
-    if player.x > G.getWidth() / 2 and player.x < playerCamera.m1x then camera.x = player.x - G.getWidth() / 2 end
-    if player.y > G.getHeight() / 2 and player.y < playerCamera.m1y then camera.y = player.y - G.getHeight() / 2 end
-  elseif m2 then
-    if player.x > G.getWidth() / 2 and player.x < playerCamera.m2x then camera.x = player.x - G.getWidth() / 2 end
-    if player.y > G.getHeight() / 2 and player.y < playerCamera.m2y then camera.y = player.y - G.getHeight() / 2 end
-  elseif m3 then
-    if player.x > G.getWidth() / 2 and player.x < playerCamera.m3x then camera.x = player.x - G.getWidth() / 2 end
-    if player.y > G.getHeight() / 2 and player.y < playerCamera.m3y then camera.y = player.y - G.getHeight() / 2 end
-  elseif mA then
-    if player.x > G.getWidth() / 2 and player.x < playerCamera.mAx then camera.x = player.x - G.getWidth() / 2 end
-    if player.y > G.getHeight() / 2 and player.y < playerCamera.mAy then camera.y = player.y - G.getHeight() / 2 end
-  end
+    if player.x > G.getWidth() / 2 and player.x < getWidthMap() - G.getWidth() / 2 then camera.x = player.x - G.getWidth() / 2 end
+    if player.y > G.getHeight() / 2 and player.y < getHeightMap() - G.getHeight() / 2 then camera.y = player.y - G.getHeight() / 2 end
 end
 
 function player:addMove()
