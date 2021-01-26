eae59a0b9d9256cae46d1e6d6b3ec67abb85f964
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2021-01-26 22:08:39 +0100

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -2,7 +2,7 @@ require "Sprite"
 require "Zombie"
 require "Shop"
 
-local lg = love.graphics
+local G = love.graphics
 
 local lastAlertTime = 0
 isAlive = true
@@ -19,15 +19,15 @@ local coin
 
 function UIDraw()
   health.drawHearts()
-  lg.draw(shop2, 320, 550)
-  lg.draw(coin, 1210, 12)
-  lg.setNewFont(28)
-  lg.print(monets, 1150, 20)
-  lg.print(alert, 10, 500)
-  lg.print(zombiesString .. #zombies, 900, 20)
-  lg.setNewFont(40)
-  lg.print(waveString .. wave.currentWave, 1000, 650)
-  lg.print(killsString .. kills, 50, 650)
+  G.draw(shop2, 320, 550)
+  G.draw(coin, 1210, 12)
+  G.setNewFont(28)
+  G.print(monets, 1150, 20)
+  G.print(alert, 10, 500)
+  G.print(zombiesString .. #zombies, 900, 20)
+  G.setNewFont(40)
+  G.print(waveString .. wave.currentWave, 1000, 650)
+  G.print(killsString .. kills, 50, 650)
 
   alertResult = (love.timer.getTime() - lastAlertTime) * 1000
   if alertResult >= 2000 then
@@ -36,9 +36,9 @@ function UIDraw()
   end
 
   if(shop_open) then
-    lg.draw(shop,0,0)
+    G.draw(shop,0,0)
   else
-    lg.draw(shop,1500,0)
+    G.draw(shop,1500,0)
   end
 
   if not isAlive and not isWinner then
@@ -53,8 +53,8 @@ function UIDraw()
 end
 
 function UILoad()
-  shop = lg.newImage('Sprites/shop.png')
-  shop2 = lg.newImage('Sprites/shop2.png')
-  coin = lg.newImage('Sprites/coin.png')
-  lg.setNewFont(28)
+  shop = G.newImage('Sprites/shop.png')
+  shop2 = G.newImage('Sprites/shop2.png')
+  coin = G.newImage('Sprites/coin.png')
+  G.setNewFont(28)
 end
