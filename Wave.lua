68495a4dbbfbc33eb46d6d3f928b3c1e888c8372
Kwolik <kwolikzsp6@gmail.com>
2021-01-26 15:04:24 +0100

    Add visual zombie in game

--- Wave.lua
+++ Wave.lua
@@ -51,7 +51,7 @@ function Wave()
     -- spawn stronger zombies
     spawnZombies(
       w - 3,
-      defaultZombieImg,
+      strongZombieImg,
       80,
       calcHP(200, w),
       200,
@@ -62,7 +62,7 @@ function Wave()
     if self.currentWave >= 5 then
       spawnZombies(
       w,
-      defaultZombieImg,
+      strongZombieImg,
       80,
       calcHP(200, w),
       calcDist(2000, w),
@@ -73,7 +73,7 @@ function Wave()
     -- spawn faster zombies
     spawnZombies(
       w - 5,
-      defaultZombieImg,
+      fastZombieImg,
       calcSpeed(180, w, 50),
       100,
       calcDist(1200, w),
@@ -82,7 +82,7 @@ function Wave()
     -- SHOOTING
     spawnZombies(
       math.floor(w / 2),
-      defaultZombieImg,
+      shootZombieImg,
       calcSpeed(60, w, 50),
       200,
       calcDist(300, w),
