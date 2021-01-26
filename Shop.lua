73fb44bab874adcf9dcc112b9caa2ea1f5ae4379
san <upsu90@gmail.com>
2021-01-26 20:18:20 +0100

    Play sounds effects during drinking potions

--- Shop.lua
+++ Shop.lua
@@ -4,6 +4,10 @@ require "Player"
 function Shop()
   local self = {
     open_shop = false,
+    healthSoundEffect = love.audio.newSource("Audio/HealthPotion.mp3", "static"),
+    speedSoundEffect = love.audio.newSource("Audio/SpeedPotion.mp3", "static"),
+    strengthSoundEffect = love.audio.newSource("Audio/StrengthPotion.mp3", "static"),
+    sniperSoundEffect = love.audio.newSource("Audio/SniperPotion.mp3", "static"),
   }
 
   function self.addMonets(money)
@@ -21,6 +25,7 @@ function Shop()
     if love.keyboard.isDown('1') then
       function love.keyreleased(key)
         if key == "1" and monets >= 250 and health.current < maxHealth then
+          self.healthSoundEffect:play()
           health.addLife()
           monets = monets - 250
           alert = "+1 health"
@@ -35,6 +40,7 @@ function Shop()
     if love.keyboard.isDown('2') then
       function love.keyreleased(key)
         if key == "2" and monets >= 250 and player.speed < 350 then
+          self.speedSoundEffect:play()
           player:addMove()
           monets = monets - 250
           alert = "+10 speed"
@@ -49,6 +55,7 @@ function Shop()
     if love.keyboard.isDown('3') then
       function love.keyreleased(key)
         if key == "3" and monets >= 300 and player.damage < 200 then
+          self.strengthSoundEffect:play()
           player:addDamage()
           monets = monets - 300
           alert = "+20 damage"
@@ -63,6 +70,7 @@ function Shop()
     if love.keyboard.isDown('4') then
       function love.keyreleased(key)
         if key == "4" and monets >= 300 and speedBullet < 600 then
+          self.speedSoundEffect:play()
           addSpeedBullet()
           monets = monets - 300
           alert = "+25 speed bullet"
