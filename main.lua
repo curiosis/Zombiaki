82226735fa6551e6bb761e904f8eec30ccb84807
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2021-01-26 03:33:36 +0100

    Move draw to func

--- main.lua
+++ main.lua
@@ -38,39 +38,7 @@ end
 function love.draw()
   if start then
     startDraw()
-    health.drawHearts()
-    lg.draw(shop2, 320, 550)
-    lg.draw(coin, 1210, 12)
-    lg.setNewFont(28)
-    lg.print(monets, 1150, 20)
-    lg.print(alert, 10, 500)
-    lg.print(zombiesString .. #zombies, 900, 20)
-    lg.setNewFont(40)
-    lg.print(waveString .. wave.currentWave, 1000, 650)
-    lg.print(killsString .. kills, 50, 650)
-
-    alertResult = (love.timer.getTime() - lastAlertTime) * 1000
-    if alertResult >= 2000 then
-      lastAlertTime = love.timer.getTime()
-      alert = ""
-    end
-
-    if(shop_open) then
-      lg.draw(shop,0,0)
-    else
-      lg.draw(shop,1500,0)
-    end
-
-    if not isAlive and not isWinner then
-      gameOverDraw()
-      if love.keyboard.isDown('space') then
-        currentHealth = maxHealth
-        health = Health()
-        startLoad()
-        isAlive = true
-      end
-    end
-
+    UIDraw()
   elseif about then
     menuDraw()
     aboutDraw()
@@ -78,4 +46,39 @@ function love.draw()
     menuDraw()
   end
 
+end
+
+function UIDraw()
+  health.drawHearts()
+  lg.draw(shop2, 320, 550)
+  lg.draw(coin, 1210, 12)
+  lg.setNewFont(28)
+  lg.print(monets, 1150, 20)
+  lg.print(alert, 10, 500)
+  lg.print(zombiesString .. #zombies, 900, 20)
+  lg.setNewFont(40)
+  lg.print(waveString .. wave.currentWave, 1000, 650)
+  lg.print(killsString .. kills, 50, 650)
+
+  alertResult = (love.timer.getTime() - lastAlertTime) * 1000
+  if alertResult >= 2000 then
+    lastAlertTime = love.timer.getTime()
+    alert = ""
+  end
+
+  if(shop_open) then
+    lg.draw(shop,0,0)
+  else
+    lg.draw(shop,1500,0)
+  end
+
+  if not isAlive and not isWinner then
+    gameOverDraw()
+    if love.keyboard.isDown('space') then
+      currentHealth = maxHealth
+      health = Health()
+      startLoad()
+      isAlive = true
+    end
+  end
 end
